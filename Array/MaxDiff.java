05f89f2a0c8f39f5e1666be3d043579439970b62
RajpootNeha <rajpoot0211neha@gmail.com>
2018-08-07 14:53:37 +0530

    Array Program

---
+++ Array/MaxDiff.java
@@ -0,0 +1,15 @@
+//n*n
+import java.util.*;
+class MaxDiff
+{
+	public static void main(String args[])
+	{
+	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	int []arr=new int(20);
+	String s;
+	int i;
+	for(i=0;i<n;i++)
+	{
+	}
+	}
+}
---
+++ Array/maxDiff1.cpp
@@ -0,0 +1,33 @@
+
+//n,n
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[100];
+	int n,i,j,max=0,small=999,c=0;
+	cin>>n;
+	for(i=0;i<n;i++)
+	{
+	cin>>a[i];
+	}
+	for(i=0;i<n;i++)
+	{
+		
+		if(small>a[i] )
+		{
+		small=a[i];
+		c=i;
+		}
+		if(max<a[i] && c<i)
+		{
+		max=a[i];
+		}
+		}
+	
+	cout<<"small : "<<small<<endl;
+	cout<<"Max : "<<max<<endl;
+	cout<<"Diff : "<<max-small<<endl;
+}
+
