a9c664799c9c3f3290637471868d78e65cfc1295
RajpootNeha <rajpoot0211neha@gmail.com>
2018-08-07 07:00:14 +0530

    added simple program of CLI

---
+++ cli.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class cli
+{
+	public static void main(String args[])
+	{
+	if(args.length>0)
+	{
+	System.out.println("The Command line arguements are : ");
+	for(String val:args)
+	{
+	System.out.println(val);
+	}
+	}
+	else
+	{
+	System.out.println("No Command line arguement :");
+	}
+	}
+}
+//zscalar
