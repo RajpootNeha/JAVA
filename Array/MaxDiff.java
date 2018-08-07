751a68a679f45d2bcafd40eeeed699501062a9dc
RajpootNeha <rajpoot0211neha@gmail.com>
2018-08-07 15:10:42 +0530

    Array Program

--- Array/MaxDiff.java
+++ Array/MaxDiff.java
@@ -2,14 +2,31 @@
 import java.util.*;
 class MaxDiff
 {
-	public static void main(String args[])
+	public static void main(String args[])throws IOException
 	{
 	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-	int []arr=new int(20);
-	String s;
-	int i;
+	int n=br.read();
+	int i,max=0,small=999,c=0,dif;
+	int a[]=new int[20];
 	for(i=0;i<n;i++)
 	{
+	a[i]=br.read();
 	}
+	for(i=0;i<n;i++)
+	{
+		
+		if(small>a[i] )
+		{
+		small=a[i];
+		c=i;
+		}
+		if(max<a[i] && c<i)
+		{
+		max=a[i];
+		}
+		}
+		dif=max=small;
+			System.out.println("Small : "+small+"\nMax : "+max+"\nDiff : "+dif);
 	}
+
 }
---
+++ Shape.java
@@ -0,0 +1,36 @@
+import java.util.*;
+abstract class Shape
+{
+	String color;
+	boolean filled;
+	Shape()
+	{
+	}
+	Shape(String color,boolean filled)
+	{
+	}
+	String getColor()
+	{
+	}
+	void setColor(String color)
+	{
+	}
+	boolean isFilled()
+	{
+	}
+	void setFilled(boolean filled)
+	{
+	}
+	double getArea()
+	{
+	}
+	double getPerimeter
+	{
+	}
+	String toString()
+	{
+	}
+}
+class Circle extends Shape
+{
+}
