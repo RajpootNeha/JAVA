c0865b3049f656af13ebdd482a5e8f9e8913d17e
RajpootNeha <rajpoot0211neha@gmail.com>
2018-09-01 15:49:10 +0530

    Algorithm

---
+++ algorithm/knapsack.cpp
@@ -0,0 +1,10 @@
+/*
+	Knapsack Problem
+	
+	Given a rucksack of capacity W and N item with their weights and values,put these items in the rucksack,so that you get the maximum total value in the rucksack and don't exceed its weight limit.
+	
+	value={1,4,5,7};
+	weight={1,3,4,5};
+*/
+
+
